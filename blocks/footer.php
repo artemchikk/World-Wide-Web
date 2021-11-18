37f347cfb74cb52c4d76d6b8201f2b910f3a3f35
artemchikk <73478460+artemchikk@users.noreply.github.com>
2021-11-18 22:52:15 +0300

    Add files via upload

---
+++ blocks/footer.php
@@ -0,0 +1,12 @@
+<div class="ui inverted vertical footer segment">
+    <div class="ui center aligned container">
+      <img src="/img/logo.png" class="ui centered mini image">
+      <div class="ui horizontal inverted small divided link list">
+        <a class="item" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Карта сайта</font></font></a>
+        <a class="item" href="/about.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Связаться с нами</font></font></a>
+        <a class="item" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Условия и положения</font></font></a>
+        <a class="item" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Политика конфиденциальности</font></font></a>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ blocks/header.php
@@ -0,0 +1,23 @@
+<div class="ui fixed inverted menu">
+    <div class="ui container">
+      <a href="#" class="header item">
+        <img class="logo" src="/img/logo.png"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Website </font></font></a>
+      <a href="/" class="item"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Главная</font></font></a>
+      <div class="ui simple dropdown item"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Разделы</font></font><i class="dropdown icon"></i>
+        <div class="menu">
+          <a class="item" href="/about.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Связь</font></font></a>
+          <a class="item" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ссылка</font></font></a>
+          <div class="divider"></div>
+          <div class="header"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Заголовок</font></font></div>
+          <div class="item">
+            <i class="dropdown icon"></i><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Подменю</font></font><div class="menu">
+              <a class="item" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ссылка</font></font></a>
+              <a class="item" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ссылка</font></font></a>
+            </div>
+          </div>
+          <a class="item" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ссылка</font></font></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
